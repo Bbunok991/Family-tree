cfbe075f65b7efb96ae38fa6f950105c0109ab76
Bbunok991 <104324674+Bbunok991@users.noreply.github.com>
2023-10-11 13:39:22 +0300

    Add files via upload

---
+++ Family_tree/Family_tree/FamilyMembers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Family_tree
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class FamilyMember
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public FamilyMember Father { get; set; }
+        public FamilyMember Mother { get; set; }
+        public FamilyMember MotherRelative { get; set; }
+        public FamilyMember FatherRalative { get; set; }
+        public Gender Gend { get; set; }
+        public virtual void Info(int indent = 0)
+        {
+            for (int i = 0; i < indent; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine($"Имя: {Name}, родители: {Father?.Name}, {Mother?.Name}, родственники: {MotherRelative?.Name}, {FatherRalative?.Name}");
+        }
+
+        public void ShowParent()
+        {
+            if (Father == null)
+                Console.WriteLine("Отец неизвестен");
+            else
+                Console.WriteLine("Отец:" + Father.Name);
+            Console.WriteLine("Мать:" + Mother?.Name);
+        }
+
+        public void ShowRelative()
+        {
+            Console.WriteLine("Родственник матери: " + MotherRelative?.Name);
+            Console.WriteLine("Родственник отца: " + FatherRalative?.Name);
+        }
+
+        public void ShowGrandParent()
+        {
+
+            Console.WriteLine("По отцовской линии:");
+            Console.WriteLine("Дедушка:" + Father?.Father?.Name);
+            Console.WriteLine("Бабушка:" + Father?.Mother?.Name);
+
+            Console.WriteLine("По материнской линии:");
+            Console.WriteLine("Дедушка:" + Mother?.Father?.Name);
+            Console.WriteLine("Бабушка:" + Mother?.Mother?.Name);
+        }
+    }
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember[] Children { get; set; }
+        public override void Info(int indent = 0)
+        {
+            base.Info(indent);
+            foreach (var child in Children)
+            {
+                child.Info(indent * 2);
+                //Console.WriteLine(child.Name);
+
+            }
+        }
+    }
+}
+
---
+++ Family_tree/Family_tree/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Family_tree;
+
+namespace Family_tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdultFamilyMember Inok = new AdultFamilyMember() { Name = "Инокентий" };
+
+            AdultFamilyMember Marg = new AdultFamilyMember() { Name = "Маргарита" };
+
+            AdultFamilyMember Danila = new AdultFamilyMember() { Name = "Данила" };
+
+            AdultFamilyMember Liza = new AdultFamilyMember() { Name = "Лиза" };
+
+            AdultFamilyMember Roman = new AdultFamilyMember() { Name = "Роман" };
+
+            AdultFamilyMember Katya = new AdultFamilyMember() { Name = "Катя" };
+
+            FamilyMember Ivan = new FamilyMember()
+            {
+                Name = "Иван",
+                Father = Inok,
+                Mother = Marg,
+                FatherRalative = Danila,
+                MotherRelative = Liza
+            };
+
+            Inok.Children = new FamilyMember[] { Ivan, new FamilyMember() { Name = "Вера" }, new FamilyMember() { Name = "Надежда" }, new FamilyMember() { Name = "Артем" }, new FamilyMember() { Name = "Эллин" } };
+            Marg.Children = new FamilyMember[] { Ivan, new FamilyMember() { Name = "Роман" }, new FamilyMember() { Name = "Катя" }, new FamilyMember() { Name = "Руслан" }, new FamilyMember() { Name = "Настя" } };
+
+
+
+            FamilyMember Mary = new FamilyMember()
+            {
+                Name = "Мария",
+                Father = new FamilyMember() { Name = "Инокентий2" },
+                Mother = new FamilyMember() { Name = "Маргарита" },
+                FatherRalative = Roman,
+                MotherRelative = Katya
+            };
+            FamilyMember Son = new FamilyMember() { Name = "Петя", Father = Ivan, Mother = Mary };
+
+
+            Inok.Info(2);
+            Marg.Info(2);
+
+
+            //Домашняя работа
+            //Доработайте приложение генеалогического дерева таким образом чтобы программа выводила на экран близких родственников(жену / мужа).
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
